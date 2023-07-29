84bf6d78d03cb46d2da46eb95955d8d80723f8e4
Kamil Czyzewski <kamilccc2001@gmail.com>
2023-07-29 17:14:26 +0200

    wzór modelu

--- tmrl/custom/models/DepthwiseSeparableConvBlock.py
+++ tmrl/custom/models/DepthwiseSeparableConvBlock.py
@@ -39,6 +39,63 @@ class DepthwiseSeparableConvNet(nn.Module):
 
 
 
+class DepthwiseSeparableConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(DepthwiseSeparableConv2d, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=stride, padding=1, groups=in_channels)
+        self.bn1 = nn.BatchNorm2d(in_channels)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.relu2 = nn.ReLU(inplace=True)
 
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.pointwise(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        return x
 
+class Conv3x3(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(Conv3x3, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
 
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class DeepwiseConvolutionX6(nn.Module):
+    def __init__(self, in_channels, out_channels, num_layers=6):
+        super(DeepwiseConvolutionX6, self).__init__()
+        layers = []
+        for i in range(num_layers):
+            if i == 0:
+                layers.append(DepthwiseSeparableConv2d(in_channels, out_channels))
+            else:
+                layers.append(DepthwiseSeparableConv2d(out_channels, out_channels))
+        self.layers = nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.layers(x)
+        return x
+
+class AvgPool2d(nn.Module):
+    def __init__(self, kernel_size, stride=None, padding=0):
+        super(AvgPool2d, self).__init__()
+        self.avg_pool = nn.AvgPool2d(kernel_size, stride=stride, padding=padding)
+
+    def forward(self, x):
+        x = self.avg_pool(x)
+        return x
+
+class FullyConnectedLayer(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(FullyConnectedLayer, self).__init__()
+        self.fc = nn.Linear(in_features, out_features)
+
+    def forward(self, x):
+        x = self.fc(x)
+        return x
