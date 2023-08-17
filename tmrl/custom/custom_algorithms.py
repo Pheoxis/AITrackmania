a7d822a155066491cdb6ab34de40ccd1fed5f1b4
Kamil Czyzewski <kamilccc2001@gmail.com>
2023-08-17 15:39:42 +0200

    TQC looking good? need replay buffer

--- tmrl/custom/custom_algorithms.py
+++ tmrl/custom/custom_algorithms.py
@@ -2,6 +2,9 @@
 import itertools
 from copy import deepcopy
 from dataclasses import dataclass
+from torch.nn import Module, Linear
+from torch.nn.functional import relu, logsigmoid
+from torch.distributions import Distribution, Normal
 
 # third-party imports
 import numpy as np
@@ -384,3 +387,169 @@ class SpinupSacAgent(TrainingAgent):  # Adapted from Spinup
             ret_dict["entropy_coef"] = alpha_t.item()
 
         return ret_dict
+
+@dataclass(eq=False)
+class TQCAgent(TrainingAgent):
+    observation_space: type
+    action_space: type
+    device: str = None  # device where the model will live (None for auto)
+    model_cls: type = MLPActorCritic
+    gamma: float = 0.99
+    polyak: float = 0.995
+    alpha: float = 0.2  # fixed (v1) or initial (v2) value of the entropy coefficient
+    lr_actor: float = 1e-3  # learning rate
+    lr_critic: float = 1e-3  # learning rate
+    lr_entropy: float = 1e-3  # entropy autotuning
+    learn_entropy_coef: bool = True
+    target_entropy: float = None  # if None, the target entropy is set automatically
+
+    model_nograd = cached_property(lambda self: no_grad(copy_shared(self.model)))
+
+    def __post_init__(self):
+        observation_space, action_space = self.observation_space, self.action_space
+        device = self.device or ("cuda" if torch.cuda.is_available() else "cpu")
+        model = self.model_cls(observation_space, action_space)
+        logging.debug(f" device SAC: {device}")
+        self.model = model.to(device)
+        self.model_target = no_grad(deepcopy(self.model))
+
+        # Set up optimizers for policy and q-function
+        self.pi_optimizer = Adam(self.model.actor.parameters(), lr=self.lr_actor)
+        self.q_optimizer = Adam(itertools.chain(self.model.q1.parameters(), self.model.q2.parameters()),lr=self.lr_critic)
+
+        # Set up optimizers for policy and q-function
+        self.pi_optimizer = Adam(self.model.actor.parameters(), lr=self.lr_actor)
+        self.q_optimizer = Adam(itertools.chain(self.model.q1.parameters(), self.model.q2.parameters()),
+                                lr=self.lr_critic)
+
+        if self.target_entropy is None:  # automatic entropy coefficient
+            self.target_entropy = -np.prod(action_space.shape).astype(np.float32)
+        else:
+            self.target_entropy = float(self.target_entropy)
+
+        if self.learn_entropy_coef:
+            # Note: we optimize the log of the entropy coeff which is slightly different from the paper
+            # as discussed in https://github.com/rail-berkeley/softlearning/issues/37
+            self.log_alpha = torch.log(torch.ones(1, device=self.device) * self.alpha).requires_grad_(True)
+            self.alpha_optimizer = Adam([self.log_alpha], lr=self.lr_entropy)
+        else:
+            self.alpha_t = torch.tensor(float(self.alpha)).to(self.device)
+
+    def get_actor(self):
+        return self.model_nograd.actor
+
+    def train(self, batch):
+        state, action, next_state, reward, not_done = replay_buffer.sample(batch_size)
+        alpha = torch.exp(self.log_alpha)
+
+    # --- Q loss ---
+        with torch.no_grad():
+        # get policy action
+            new_next_action, next_log_pi = self.actor(next_state)
+
+        # compute and cut quantiles at the next state
+            next_z = self.critic_target(next_state, new_next_action)  # batch x nets x quantiles
+            sorted_z, _ = torch.sort(next_z.reshape(batch, -1))
+            sorted_z_part = sorted_z[:, :self.quantiles_total - self.top_quantiles_to_drop]
+
+        # compute target
+            target = reward + not_done * self.discount * (sorted_z_part - alpha * next_log_pi)
+
+        cur_z = self.critic(state, action)
+        critic_loss = self.quantile_huber_loss_f(cur_z, target)
+
+        # --- Policy and alpha loss ---
+        new_action, log_pi = self.actor(state)
+        alpha_loss = -self.log_alpha * (log_pi + self.target_entropy).detach().mean()
+        actor_loss = (alpha * log_pi - self.critic(state, new_action).mean(2).mean(1, keepdim=True)).mean()
+
+        # --- Update ---
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+            target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        self.alpha_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.alpha_optimizer.step()
+
+        self.total_it += 1
+    def quantile_huber_loss_f(self,quantiles, samples):
+        pairwise_delta = samples[:, None, None, :] - quantiles[:, :, :, None]  # batch x nets x quantiles x samples
+        abs_pairwise_delta = torch.abs(pairwise_delta)
+        huber_loss = torch.where(abs_pairwise_delta > 1,
+                                 abs_pairwise_delta - 0.5,
+                                 pairwise_delta ** 2 * 0.5)
+
+        n_quantiles = quantiles.shape[2]
+        tau = torch.arange(n_quantiles, device = self.device or ("cuda" if torch.cuda.is_available() else "cpu")).float() / n_quantiles + 1 / 2 / n_quantiles
+        loss = (torch.abs(tau[None, None, :, None] - (pairwise_delta < 0).float()) * huber_loss).mean()
+        return loss
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Mlp(Module):
+        def __init__(
+                self,
+                input_size,
+                hidden_sizes,
+                output_size
+        ):
+            super().__init__()
+            # TODO: initialization
+            self.fcs = []
+            in_size = input_size
+            for i, next_size in enumerate(hidden_sizes):
+                fc = Linear(in_size, next_size)
+                self.add_module(f'fc{i}', fc)
+                self.fcs.append(fc)
+                in_size = next_size
+            self.last_fc = Linear(in_size, output_size)
+
+        def forward(self, input):
+            h = input
+            for fc in self.fcs:
+                h = relu(fc(h))
+            output = self.last_fc(h)
+            return output
+
+    class TanhNormal(Distribution):
+        def __init__(self, normal_mean, normal_std):
+            super().__init__()
+            self.normal_mean = normal_mean
+            self.normal_std = normal_std
+            self.standard_normal = Normal(torch.zeros_like(self.normal_mean, device=self.device or ("cuda" if torch.cuda.is_available() else "cpu")),
+                                          torch.ones_like(self.normal_std, device=self.device or ("cuda" if torch.cuda.is_available() else "cpu")))
+            self.normal = Normal(normal_mean, normal_std)
+
+        def log_prob(self, pre_tanh):
+            log_det = 2 * np.log(2) + logsigmoid(2 * pre_tanh) + logsigmoid(-2 * pre_tanh)
+            result = self.normal.log_prob(pre_tanh) - log_det
+            return result
+
+        def rsample(self):
+            pretanh = self.normal_mean + self.normal_std * self.standard_normal.sample()
+            return torch.tanh(pretanh), pretanh
